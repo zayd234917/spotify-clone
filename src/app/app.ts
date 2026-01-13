823630633f4a8a7a4b2c26484bbb4b7404d0e054
zayd234917 <zaydbenkhaira@gmail.com>
2026-01-13 14:48:25 +0000

    complet partition the site

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SideBar } from "./components/side-bar/side-bar";
+import { Footer } from "./components/footer/footer";
+import { Home } from "./pages/home/home";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, SideBar, Footer, Home],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
--- src/app/components/footer/footer.ts
+++ src/app/components/footer/footer.ts
@@ -1,11 +1,19 @@
 import { Component } from '@angular/core';
-
+import { OnInit } from '@angular/core';
 @Component({
   selector: 'app-footer',
   imports: [],
   templateUrl: './footer.html',
   styleUrl: './footer.css',
 })
-export class Footer {
-
+export class Footer implements OnInit{
+     playing : boolean = true;
+     value! : string; 
+     ngOnInit(){
+      if(this.playing){
+         this.value = "Stop"
+      }else{
+        this.value = "play"
+      }
+     }
 }
